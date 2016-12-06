aefec53fed020bbbbab8e0e46696a2b0b7e72550
Jessica Asuncion <jess.asuncion93@gmail.com>
2016-12-06 08:52:41 -0500

    Modified fixed container logic to hide during initial page load

--- js/actions.js
+++ js/actions.js
@@ -8,9 +8,15 @@ $(window).on('load', function(){
 
 $(window).scroll(function (event) {
     var windowTop = $(this).scrollTop();        
-    if (windowTop <= $(".photography").offset().top) {
-        $(".skills-container").addClass("fixed");
-    } else {
+    // if (windowTop <= $(".photography").offset().top) {
+    //     $(".skills-container").addClass("fixed");
+    // } else {
+    //     $(".skills-container").removeClass("fixed");
+    // }
+
+    if (windowTop < $(".about").offset().top || windowTop > $(".photography").offset().top) {
         $(".skills-container").removeClass("fixed");
+    } else {
+        $(".skills-container").addClass("fixed");
     }
 });
