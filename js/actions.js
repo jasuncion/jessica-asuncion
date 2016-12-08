6d37379e2ec1d4540a032f49c151b3e186729d0d
Jessica Asuncion <jess.asuncion93@gmail.com>
2016-12-07 21:59:54 -0500

    Modified about content to fade in from right

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,10 @@
+var $window = $(window);
+
+function maxImage(x) {
+  $(".display-box").css('display', 'initial');
+  $(".display-box").append("<img src=\"images/photography/portfolio" + x + ".jpg\">");
+}
+
 $(document).ready(function() {
   $(".container").shapeshift();
 
@@ -13,14 +20,17 @@ $(window).on('load', function(){
 $(window).scroll(function (event) {
     var windowTop = $(this).scrollTop();        
 
-    if (windowTop < $(".main").offset().top + 50 || windowTop > $(".photography").offset().top) {
+    if (windowTop < $(".main").offset().top || windowTop > $(".photography").offset().top) {
         $(".skills-container").removeClass("fixed");
     } else {
         $(".skills-container").addClass("fixed");
     }
-});
 
-function maxImage(x) {
-  $(".display-box").css('display', 'initial');
-  $(".display-box").append("<img src=\"images/photography/portfolio" + x + ".jpg\">");
-}
+    if (windowTop > $(".main").offset().top && windowTop < $(".technical").offset().top) {
+      $('.about-content').addClass('in-view');
+      $('.about-content').removeClass('animation-element');
+    } else {
+      $('.about-content').removeClass('in-view');
+      $('.about-content').addClass('animation-element');
+    }
+});
